30d79b92c6d42b8f4b268bcf747a8c2b2fc5e9f1
Donovan Davis <dt-davis@Hotmail.com>
2020-05-14 22:19:09 -0500

    fixed array declaration added basic html structure added javascript function for sumbit todo css

--- 02-tallest-mountain/main.js
+++ 02-tallest-mountain/main.js
@@ -9,16 +9,33 @@
 */
 
 // YOUR CODE HERE
-mntValue =[ 120 , 200 , 3000, 6400, 1200, 50]
+const submitButton = document.querySelector('.userSubmit')
+const userMntvalues = document.querySelector('.userInputs')
+const resultText = document.querySelector('.results')
+mntValue = [120, 200, 3000, 6400, 1200, 50]
 
 let index = 0
 let highestMnt = 0
 
-while (index < mntValue.length){
-   if ( mntValue[index] > highestMnt){
+while (index < mntValue.length) {
+   if (mntValue[index] > highestMnt) {
       highestMnt = mntValue[index]
    }
-   index ++
+   index++
 }
 
 console.log(highestMnt)
+let counter = 0
+submitButton.addEventListener('click', function () {
+   index = 0
+   mntValue.push(userMntvalues.value)  
+   counter++
+   while (index < mntValue.length) {
+      if (mntValue[index] > highestMnt) {
+         highestMnt = mntValue[index]
+      }
+      index++
+   }
+   resultText.innerHTML = highestMnt
+   console.log(mntValue.length)
+})
